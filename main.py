a05be090d446812d9637d639ea1ba93658182f3a
Carlos Arturo Guerrero Castillo <70684435+carlosSploit@users.noreply.github.com>
2023-06-27 21:47:08 -0500

    Update main.py

--- main.py
+++ main.py
@@ -177,6 +177,4 @@ def webhook_whatsapp():
     return jsonify({"status": "success"}, 200)
 
 if __name__ == "__main__":
-    print(os.getenv('TOKEN'))
-    print(os.getenv('IDWHATSAPP'))
     app.run(debug = True)
