b6e8030e1acd86c55e3882a750a6fb12964d785f
carlosSploit <arturo14212000@gmail.com>
2023-06-30 01:37:07 -0500

    30/06/2023

--- history.py
+++ history.py
@@ -7,8 +7,7 @@ def historyPeticion(telefonoCliente, mensajes, response):
     if (fe.exists()):
         with open("text.txt", "a") as filetextw:
             fechaActual = datetime.now()
-            fechaFormat = datetime.strftime(
-                fechaActual, '%b %d %Y %H:%M:%S')
+            fechaFormat = datetime.strftime(fechaActual, '%b %d %Y %H:%M:%S')
             historyFormat = "time: {0}|telefonoCliente: {1}|messege: {2}|response: {3}\n".format(
                 fechaFormat, telefonoCliente, mensajes, response['messeg'].replace("\n", "$"))
             filetextw.write(historyFormat)
